10da532064b3cacda0838bd36437fc833e341d80
Tan Vu <lap>
2021-02-23 11:25:11 +0700

    up

---
+++ ModalWindowEffects/js/modal.js
@@ -0,0 +1,50 @@
+/**
+ * modalEffects.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var ModalEffects = (function() {
+
+	function init() {
+		var overlay = document.querySelector( '.md-overlay' );
+
+		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
+				close = modal.querySelector( '.md-close' );
+
+			function removeModal( hasPerspective ) {
+				modal.classList.remove('md-show');
+				if( hasPerspective ) {
+					document.documentElement.classList.remove('md-perspective');
+				}
+			}
+
+			function removeModalHandler() {
+				removeModal( el.classList.contains('md-setperspective') ); 
+			}
+
+			el.addEventListener( 'click', function( ev ) {
+				modal.classList.add('md-show');
+				overlay.removeEventListener( 'click', removeModalHandler );
+				overlay.addEventListener( 'click', removeModalHandler );
+
+				if( el.classList.contains('md-setperspective') ) {
+					setTimeout( function() {
+						document.documentElement.classList.add('md-perspective');
+					}, 25 );
+				}
+			});
+
+			close.addEventListener( 'click', function( ev ) {
+				ev.stopPropagation();
+				removeModalHandler();
+			});
+		} );
+	}
+	init();
+})();
